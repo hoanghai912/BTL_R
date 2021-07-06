672346a951716aa61924c4c21201fe00cd39d2c8
unknown <admin>
2021-07-07 00:53:15 +0700

    laptop2

--- BTL.R
+++ BTL.R
@@ -5,5 +5,3 @@ data <- read.csv('C:/Users/Admin/Desktop/XSTK/gia_nha.csv', TRUE, ",")
 new_DF <- data.frame(data$price, data$sqft_living15, data$floors,
                      data$condition, data$sqft_above, data$sqft_living)
 
-new_DF %>% summary
-
