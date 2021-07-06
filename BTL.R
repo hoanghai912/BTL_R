9f233bcde0c5421c31e07edee68da8ff91c5843b
Hai <admin>
2021-07-06 20:27:52 +0700

    section2

--- BTL.R
+++ BTL.R
@@ -5,4 +5,3 @@ data <- read.csv('C:/Users/Administrator/Desktop/SXTK/gia_nha.csv', TRUE, ",")
 new_DF <- data.frame(data$price, data$sqft_living15, data$floors,
                      data$condition, data$sqft_above, data$sqft_living)
 
-new_DF %>% str
