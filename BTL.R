cefe86e2da839124f328470759143cbfe11796af
Hai <admin>
2021-07-07 02:55:50 +0700

    replace NA in price

--- BTL.R
+++ BTL.R
@@ -5,3 +5,5 @@ data <- read.csv('C:/Users/Administrator/Desktop/SXTK/gia_nha.csv', TRUE, ",")
 new_DF <- data.frame(data$price, data$sqft_living15, data$floors,
                      data$condition, data$sqft_above, data$sqft_living)
 
+new_DF[is.na(new_DF)] <- 0
+which(is.na(new_DF))
