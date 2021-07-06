862860c9860cb238a2268619150eb0081c04ce66
Hai <admin>
2021-07-06 20:19:46 +0700

    Section1

---
+++ BTL.R
@@ -0,0 +1,8 @@
+library(dplyr)
+
+data <- read.csv('C:/Users/Administrator/Desktop/SXTK/gia_nha.csv', TRUE, ",")
+
+new_DF <- data.frame(data$price, data$sqft_living15, data$floors,
+                     data$condition, data$sqft_above, data$sqft_living)
+
+new_DF %>% str
